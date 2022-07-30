9077503ca2b5ac26918354e34b649e36a4499525
Huso-ops <81871561+Huso-ops@users.noreply.github.com>
2022-07-30 18:10:19 +0300

    MD without mongoose
    MD without mongoose

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const phoneRoute = require("./route/phoneRoute");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+
+
+
+app.get("/", (req, res) => {
+
+    try {
+
+      res.status(200).json({ message: "Api is working! Say hello" });
+    } catch (error) {
+
+      res.status(500).json({ message: error });
+
+    }
+  });
+
+
+  dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.options("*", cors());
+app.use(bodyParser.json());
+
+app.use("/api/phone",phoneRoute);
+
+let db;
+
+MongoClient
+  .connect(process.env.MONGO_URL,
+     { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, client) {
+      db = client.db()
+   
+  console.log("mongodb Connect")
+    }
+  )
+
+
+/*
+let db;
+
+let connectionString = "mongodb+srv://huso:3rcGPzoUA6UR6sSi@nestjs.77vms.mongodb.net/redis?retryWrites=true&w=majority";
+
+MongoClient.connect(
+    connectionString,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, client) {
+      db = client.db()
+   
+  console.log("mongodb Connect")
+    }
+  )
+
+  */
+
+
+  app.listen(process.env.PORT || 8080, () => {
+    console.log("server running at " + process.env.PORT );
+  }
+     
+  );
+
+
